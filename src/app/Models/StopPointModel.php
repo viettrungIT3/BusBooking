69c60083c8deab678914f29c328c5da00aad1cd6
viettrungIT3 <viettrungcntt03@gmail.com>
2024-03-30 15:49:24 +0700

    feat(Model): :sparkles: create StopPointModel

---
+++ src/app/Models/StopPointModel.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StopPointModel extends Model
+{
+    protected $table = 'stop_points';
+
+    protected $allowedFields = [
+        'schedule_id',
+        'name',
+        'arrival_time',
+        'sequence'
+    ];
+}
