ff264d6059b3bce74e2e709621e710a0c12376a9
viettrungIT3 <viettrungcntt03@gmail.com>
2024-03-31 19:28:48 +0700

    feat(FE): :sparkles: use lib daterangepicker to filter date for page admin/manage-schedules (list schedules)

--- src/app/Views/backend/manage-schedules/index.php
+++ src/app/Views/backend/manage-schedules/index.php
@@ -10,6 +10,13 @@
 	href="<?php echo base_url() . '/plugins/AdminLTE-3.2.0/'; ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet"
 	href="<?php echo base_url() . '/plugins/AdminLTE-3.2.0/'; ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<!-- Include Date Range Picker -->
+<link rel="stylesheet" href="<?php echo base_url('plugins/daterangepicker/daterangepicker.css') ?>">
+
+<!-- js -->
+<script src="<?= base_url("/plugins/AdminLTE-3.2.0/") ?>plugins/moment/moment.min.js"></script>
+<script src="<?= base_url("/plugins/AdminLTE-3.2.0/") ?>plugins/inputmask/jquery.inputmask.min.js"></script>
+<script src="<?php echo base_url('plugins/daterangepicker/daterangepicker.js') ?>"></script>
 
 <div class="content-wrapper">
 	<div class="content-header">
@@ -60,11 +67,63 @@
 			<!-- DataTales Example -->
 			<!-- Log on to codeastro.com for more projects -->
 			<div class="card shadow mb-4">
-				<div class="card-header py-3">
-					<a href="<?= base_url('/admin/manage-schedules/create-schedule') ?>" type="button"
-						class="btn btn-success pull-right">
-						Thêm lịch trình
-					</a>
+				<div class="card-header">
+					<h2 class="card-title">
+						<div id="time-filer" class="pull-right"
+							style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+							<i class="glyphicon glyphicon-calendar fa fa-calendar"></i>&nbsp;
+							<span></span> <b class="caret"></b>
+						</div>
+
+						<script type="text/javascript">
+							$(function () {
+
+								const currentUrlParams = new URLSearchParams(window.location.search);
+								let start = currentUrlParams.has('startDate') ? moment(currentUrlParams.get('startDate')) : moment();
+								let end = currentUrlParams.has('endDate') ? moment(currentUrlParams.get('endDate')) : moment();
+
+
+								function cb(start, end) {
+									$('#time-filer span').html(start.format('YYYY-MM-DD') + ' --> ' + end.format('YYYY-MM-DD'));
+
+									const currentStart = currentUrlParams.get('startDate');
+									const currentEnd = currentUrlParams.get('endDate');
+
+									// Chỉ tải lại trang nếu có sự khác biệt trong lựa chọn thời gian
+									if (start.format('YYYY-MM-DD') !== currentStart || end.format('YYYY-MM-DD') !== currentEnd) {
+										window.location.href = `${window.location.pathname}?startDate=${start.format('YYYY-MM-DD')}&endDate=${end.format('YYYY-MM-DD')}`;
+									}
+								}
+
+
+
+
+								$('#time-filer').daterangepicker({
+									startDate: start,
+									endDate: end,
+									ranges: {
+										'Hôm nay': [moment(), moment()],
+										'Hôm qua': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+										'Ngày mai': [moment().add(1, 'days'), moment().add(1, 'days')],
+										'7 ngày trước': [moment().subtract(6, 'days'), moment()],
+										'7 ngày tới': [moment(), moment().add(6, 'days')],
+										'Tháng này': [moment().startOf('month'), moment().endOf('month')],
+										'Tháng trước': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+									}
+								}, cb);
+
+								cb(start, end);
+
+							});
+						</script>
+
+					</h2>
+					<div class="card-tools">
+						<a href="<?= base_url('/admin/manage-schedules/create-schedule') ?>" type="button"
+							class="btn btn-success pull-right">
+							Thêm lịch trình
+						</a>
+					</div>
 				</div>
 				<div class="card-body">
 					<div class="table-responsive">
@@ -100,10 +159,10 @@
 											<?= $row->origin . ' --> ' . $row->destination; ?>
 										</td>
 										<td>
-											<?= date('H:i', strtotime($row->departure_time)); ?>
+											<?= date('Y-m-d H:i', strtotime($row->departure_time)); ?>
 										</td>
 										<td>
-											<?= date('H:i', strtotime($row->arrival_time)); ?>
+											<?= date('Y-m-d H:i', strtotime($row->arrival_time)); ?>
 										</td>
 										<td class="text-end">
 											<?= number_format((float) ($row->price), 0, ",", "."); ?> VNĐ
