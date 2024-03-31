25e8b041005cb5ac59d9b383e5305e9cefd2defa
viettrungIT3 <viettrungcntt03@gmail.com>
2024-03-31 22:58:05 +0700

    feat(FE): :sparkles: feature clones schedule with startDate and endDate
    add btn copy,  add modal copy

--- src/app/Views/backend/manage-schedules/index.php
+++ src/app/Views/backend/manage-schedules/index.php
@@ -170,21 +170,28 @@
 										<td>
 											<?= $row->stop_points; ?>
 										</td>
-										<td class="center text-nowrap">
+										<td class="text-center">
 											<a href="<?= base_url('admin/manage-schedules/show-schedule/' . $row->id) ?>"
-												class="btn btn-primary btn-sm" title="Xem">
+												class="btn btn-primary btn-sm mb-1" title="Xem">
 												<i class="fas fa-eye"></i>
 												<span class="text-xxl-visible">Xem</span>
 											</a>
 											<a href="<?= base_url("admin/manage-schedules/update-schedule/" . $row->id) ?>"
-												class="btn btn-info btn-sm" title="Sửa">
+												class="btn btn-info btn-sm mb-1" title="Sửa">
 												<i class="fas fa-pencil-alt"></i>
 												<span class="text-xxl-visible">Sửa</span>
 											</a>
 											<a href="<?= base_url("admin/manage-schedules/delete-schedule/" . $row->id) ?>"
-												class="btn btn-danger btn-sm" title="Xóa">
+												class="btn btn-danger btn-sm mb-1" title="Xóa">
 												<i class="fas fa-trash"></i>
 												<span class="text-xxl-visible">Xóa</span>
+											</a>
+											<!-- Nút mở modal -->
+											<a href="#copyScheduleModal" data-toggle="modal" data-id="<?= $row->id ?>"
+												class="btn btn-secondary btn-sm mb-1 open-copyScheduleModal"
+												title="Tạo bản sao">
+												<i class="fas fa-copy"></i>
+												<span class="text-xxl-visible">Nhân bản</span>
 											</a>
 										</td>
 									</tr>
@@ -199,6 +206,57 @@
 		<!-- /.container-fluid -->
 	</section>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="copyScheduleModal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+	aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Tạo bản sao lịch trình</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form id="copyScheduleForm" action="<?= base_url('admin/manage-schedules/copy-schedule') ?>" method="post">
+				<div class="modal-body">
+					<div class="callout callout-info">
+						<h5><i class="fas fa-info"></i> Lưu ý:</h5>
+						<ul class="mb-0">
+							<li>
+								Khoảng thời gian được chọn sẽ áp dụng cho tất cả các điểm dừng và thông tin lịch trình
+								của bản sao.
+							</li>
+							<li>
+								Bản sao lịch trình sẽ được tạo với tất cả thông tin giống hệt lịch trình gốc, bao gồm cả
+								các điểm dừng và giá vé, nhưng với khoảng thời gian mới được chỉ định.
+							</li>
+							<li>
+								Hãy đảm bảo rằng khoảng thời gian mới không gây ra xung đột lịch trình với các chuyến đi
+								khác.
+							</li>
+							<li>
+								Việc tạo bản sao lịch trình nên được thực hiện một cách cẩn thận để tránh nhầm lẫn hoặc
+								trùng lặp không cần thiết.
+							</li>
+						</ul>
+					</div>
+
+					<input type="hidden" id="scheduleId" name="scheduleId" value="">
+					<div class="form-group">
+						<label>Chọn khoảng thời gian:</label>
+						<input type="text" class="form-control" id="dateRangePicker" name="dateRange" required>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary">Tạo bản sao</button>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
 
 <!-- DataTables  & Plugins -->
 <script
@@ -231,10 +289,49 @@
 				responsive: true,
 				autoWidth: false,
 				buttons: ["copy", "csv", "excel", "pdf"],
+				columnDefs: [
+					{
+						targets: -1, // Đây là cách chỉ định cột cuối cùng
+						visible: true, // Đảm bảo cột này luôn được hiển thị
+						className: 'all' // Sử dụng lớp 'all' để cột này luôn hiển thị ở tất cả các điểm phá vỡ responsive
+					}
+				]
 			})
 			.buttons()
 			.container()
 			.appendTo("#example1_wrapper .col-md-6:eq(0)");
+
+		// Khởi tạo Date Range Picker
+		$('#dateRangePicker').daterangepicker({
+			locale: {
+				format: 'YYYY-MM-DD',
+				applyLabel: "Áp dụng",
+				cancelLabel: "Hủy bỏ",
+				customRangeLabel: "Tùy chỉnh", // Thay đổi chữ "Custom Range" thành "Tùy chỉnh"
+			},
+			startDate: moment().add(1, 'days'), // Khoảng thời gian mặc định bắt đầu từ ngày hiện tại
+			endDate: moment().add(1, 'days'), // Khoảng thời gian kết thúc sau 29 ngày
+			ranges: {
+				'Ngày mai': [moment().add(1, 'days'), moment().add(1, 'days')],
+				'3 ngày tới': [moment().add(1, 'days'), moment().add(3, 'days')],
+				'7 ngày tới': [moment().add(1, 'days'), moment().add(7, 'days')],
+				'1 tháng tới': [moment().add(1, 'days'), moment().add(1, 'month')],
+				'3 tháng tới': [moment().add(1, 'days'), moment().add(3, 'months')],
+				'6 tháng tới': [moment().add(1, 'days'), moment().add(6, 'months')],
+			}
+		}, function (start, end, label) {
+			// Có thể sử dụng callback này để xử lý khi người dùng chọn một khoảng thời gian
+			console.log("Bạn đã chọn: " + start.format('YYYY-MM-DD') + ' đến ' + end.format('YYYY-MM-DD'));
+		});
+
+
+		// Khi nút mở modal được click
+		$('.open-copyScheduleModal').click(function () {
+			var scheduleId = $(this).data('id'); // Lấy ID từ attribute data-id của nút
+			$('#scheduleId').val(scheduleId); // Gán ID vào input hidden trong form// Cập nhật tiêu đề modal
+			$('#copyScheduleModal .modal-title').text('Tạo bản sao lịch trình #' + scheduleId);
+			$('#copyScheduleModal').modal('show'); // Hiển thị modal
+		});
 	});
 
 </script>
