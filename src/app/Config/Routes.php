7282c43aa8d9de7cac7cbc6bd7a8769856146a09
viettrungIT3 <viettrungcntt03@gmail.com>
2024-04-01 23:16:35 +0700

    feat(Routes): :sparkles: update route

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,17 +5,49 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/login', 'User::login');
-$routes->get('/logout', 'User::logout');
-$routes->post('/login', 'User::login');
-$routes->get('/register', 'User::register');
-$routes->post('/register', 'User::register');
-$routes->get('/bus-detail', 'WebsiteBus::detail');
-$routes->get('/tickets', 'WebsiteBus::tickets');
-$routes->get('/checkout', 'WebsiteBus::checkout');
-$routes->get('/checkout/info', 'WebsiteBus::checkout_info');
-$routes->get('/checkout/cancel', 'WebsiteBus::checkout_cancel');
+
+//  Đăng Nhập và Đăng Ký
+$routes->get('/login', 'AuthController::login');
+$routes->post('/login', 'AuthController::login');
+$routes->get('/register', 'AuthController::register');
+$routes->post('/register', 'AuthController::register');
+$routes->get('/logout', 'AuthController::logout');
+
+// Trang chủ, giới thiệu, liên hệ
+$routes->get('/', 'HomeController::index');
+$routes->get('/about', 'AboutController::index');
+$routes->get('/contact', 'ContactController::index');
+
+// Xem danh sách và chi tiết lịch trình, xe buýt, tuyến đường
+$routes->get('/schedules', 'ScheduleController::index');
+$routes->get('/schedules/view/(:num)', 'ScheduleController::view/$1');
+$routes->get('/buses', 'BusController::index');
+$routes->get('/buses/view/(:num)', 'BusController::view/$1');
+$routes->get('/routes', 'RouteController::index');
+$routes->get('/routes/view/(:num)', 'RouteController::view/$1');
+
+// Đặt vé và quản lý đặt vé
+$routes->get('bookings', 'BookingController::index');
+$routes->get('bookings/details/(:num)', 'BookingController::details/$1');
+$routes->get('bookings/create/(:num)', 'BookingController::create/$1');
+$routes->get('bookings/cancel/(:num)', 'BookingController::cancel/$1');
+$routes->post('bookings/store', 'BookingController::store');
+
+
+$routes->group('user', ['filter' => 'auth'], function ($routes) {
+    // Hồ sơ người dùng và cập nhật hồ sơ
+    $routes->get('profile', 'User\ProfileController::index');
+    $routes->post('profile/update', 'User\ProfileController::update');
+
+    // Thanh toán
+    $routes->get('payments/(:num)', 'User\PaymentController::paymentForm/$1');
+    $routes->post('payments/process', 'User\PaymentController::process');
+
+    // Gửi phản hồi
+    $routes->get('feedback', 'User\FeedbackController::index');
+    $routes->post('feedback/submit', 'User\FeedbackController::submit');
+});
+
 
 $routes->group('admin', ['namespace' => 'App\Controllers\Admin', 'filter' => 'sessionLogin'], static function ($routes) {
     // Dashboard
