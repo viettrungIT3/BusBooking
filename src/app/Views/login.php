fe239871b630dc21edb0945fdd49ec4bee24dfbb
viettrungIT3 <viettrungcntt03@gmail.com>
2024-04-30 23:38:17 +0700

    feat(login): :sparkles: update ui login

--- src/app/Views/login.php
+++ src/app/Views/login.php
@@ -1,21 +1,29 @@
-<?= $this->extend('layout') ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?= $this->section('content') ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $title ?></title>
 
-<style>
-    html,
-    body,
-    .wrapper,
-    .container {
-        height: 100% !important;
-    }
-</style>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= base_url() ?>/plugins/AdminLTE-3.2.0/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet"
+        href="<?= base_url() ?>/plugins/AdminLTE-3.2.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url() ?>/plugins/AdminLTE-3.2.0/dist/css/adminlte.min.css">
+</head>
 
-<div class="container">
-    <div class="wrapper d-flex align-items-center justify-content-center h-100">
-        <div class="card" style="min-width: 350px">
-            <div class="card-header">
-                <h4>Đăng nhập</h4>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <h1><b>BusBooking</b></h1>
             </div>
             <div class="card-body">
                 <?php if (isset($error)): ?>
@@ -30,20 +38,66 @@
                 <?php endif; ?>
 
                 <form action="/login" method="post">
-                    <div class="mb-3">
-                        <label for="identity" class="form-label">Email hoặc Số điện thoại</label>
-                        <input type="text" name="identity" class="form-control"
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="identity" placeholder="Email hoặc Số điện thoại"
                             value="<?php echo set_value('identity'); ?>">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Mật khẩu</label>
-                        <input type="password" name="password" class="form-control">
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Mật khẩu">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
                     </div>
-                    <button type="submit" class="btn btn-primary">Đăng nhập</button>
-
+                    <div class="row">
+                        <!-- <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    Remember Me
+                                </label>
+                            </div>
+                        </div> -->
+                        <!-- /.col -->
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary btn-block">Đăng nhập</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
                 </form>
+
+                <div class="social-auth-links text-center mt-2 mb-3">
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> Hoặc đăng nhập bằng Google+
+                    </a>
+                </div>
+                <!-- /.social-auth-links -->
+
+                <p class="mb-1">
+                    <a href="forgot-password.html">Quên mật khẩu</a>
+                </p>
+                <p class="mb-0">
+                    <a href="<?= base_url('register') ?>" class="text-center">Đăng ký tài khoản mới</a>
+                </p>
             </div>
+            <!-- /.card-body -->
         </div>
+        <!-- /.card -->
     </div>
-</div>
-<?= $this->endSection() ?>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="<?= base_url() ?>/plugins/AdminLTE-3.2.0/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url() ?>/plugins/AdminLTE-3.2.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url() ?>/plugins/AdminLTE-3.2.0/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
