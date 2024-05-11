ee7942364bf8e525510ad3f4263d5dc61a2339f3
viettrungIT3 <viettrungcntt03@gmail.com>
2024-05-12 04:52:20 +0700

    feat(Controller): create Admin/PaymentMethodController

---
+++ src/app/Controllers/Admin/PaymentMethodController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use App\Models\PaymentMethodModel;
+
+class PaymentMethodController extends BaseController
+{
+    private $paymentMethodModel = NULL;
+
+    public function __construct() {
+        $this->paymentMethodModel = new PaymentMethodModel();
+    }
+
+
+    public function index()
+    {
+        // Logic để hien thi danh sach phuong thức thanh toan
+    }
+
+    public function create()
+    {
+        // Logic để hiển thị form tạo mới
+    }
+
+    public function store()
+    {
+        // Logic để lưu dữ liệu form
+    }
+
+    public function edit($id)
+    {
+        // Logic để hiển thị form chỉnh sửa
+    }
+
+    public function update($id)
+    {
+        // Logic để cập nhật dữ liệu
+    }
+
+    public function delete($id)
+    {
+        // Logic để xóa phương thức thanh toán
+    }
+
+}
