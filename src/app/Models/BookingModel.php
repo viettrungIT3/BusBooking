492cfb59730b771ea194884ff0bd0c33d4c0309b
viettrungIT3 <viettrungcntt03@gmail.com>
2024-05-11 16:22:31 +0700

    feat(Model): update BookingModel

--- src/app/Models/BookingModel.php
+++ src/app/Models/BookingModel.php
@@ -3,10 +3,18 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class BusUtilityModel extends Model
+class BookingModel extends Model
 {
     protected $table = 'bookings';
     protected $primaryKey = 'id';
 
-    protected $allowedFields = ['user_id', 'schedule_id', 'book_date', 'quantity', 'status', 'payment_status'];
+    protected $allowedFields = [
+        'user_id',
+        'schedule_id',
+        'book_date',
+        'quantity',
+        'status',
+        'payment_status',
+        'notes'
+    ];
 }
