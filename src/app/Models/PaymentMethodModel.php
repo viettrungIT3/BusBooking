0787c4d3be054db1b6d514baebf5a727072c8257
viettrungIT3 <viettrungcntt03@gmail.com>
2024-05-12 17:04:47 +0700

    feat(Model): udpate PaymentMethodModel
    remove filter created_at, updated_at, delete_at

--- src/app/Models/PaymentMethodModel.php
+++ src/app/Models/PaymentMethodModel.php
@@ -12,9 +12,4 @@ class PaymentMethodModel extends Model
     protected $useSoftDeletes = false;
 
     protected $allowedFields = ['name', 'description', 'status', 'image'];
-
-    protected $useTimestamps = true;
-    protected $createdField = 'created_at';
-    protected $updatedField = 'updated_at';
-    protected $deletedField = 'deleted_at';
 }
