641a7ac46a1aaa6b210d2b190b716b3f55bae6ff
viettrungIT3 <viettrungcntt03@gmail.com>
2024-05-12 18:23:37 +0700

    feat(admin/payment-methods): feature sotre payment-methods in PaymentMethodController validate rules, upload file and save database

--- src/app/Controllers/Admin/PaymentMethodController.php
+++ src/app/Controllers/Admin/PaymentMethodController.php
@@ -3,12 +3,14 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\PaymentMethodModel;
+use CodeIgniter\Files\File;
 
 class PaymentMethodController extends BaseController
 {
     private $paymentMethodModel = NULL;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->paymentMethodModel = new PaymentMethodModel();
     }
 
@@ -34,7 +36,53 @@ class PaymentMethodController extends BaseController
 
     public function store()
     {
-        // Logic để lưu dữ liệu form
+
+        helper(['form']);
+        $rules = [
+            'name' => [
+                'rules' => 'required|min_length[3]|max_length[100]',
+                'errors' => [
+                    'required' => 'Tên là bắt buộc.',
+                    'min_length' => 'Tên phải có ít nhất {param} ký tự.',
+                    'max_length' => 'Tên không được vượt quá {param} ký tự.'
+                ]
+            ],
+            'description' => [
+                'rules' => 'required|min_length[10]|max_length[1000]',
+                'errors' => [
+                    'required' => 'Mô tả là bắt buộc.',
+                    'min_length' => 'Mô tả phải có ít nhất {param} ký tự.',
+                    'max_length' => 'Mô tả không được với quá {param} ký tự.'
+                ]
+            ],
+        ];
+
+        if ($this->validate($rules)) {
+            $file = $this->request->getFile('image');
+            $newName = NULL;
+            if ($file->isValid() && !$file->hasMoved()) {
+                $newName = 'uploads/payment-methods/' . $file->getRandomName();
+                if (!is_dir(ROOTPATH . 'public/uploads')) {
+                    mkdir(ROOTPATH . 'public/uploads', 0777, TRUE);
+                }
+                if (!is_dir(ROOTPATH . 'public/uploads/payment-methods')) {
+                    mkdir(ROOTPATH . 'public/uploads/payment-methods', 0777, TRUE);
+                }
+                $file->move(ROOTPATH . 'public', $newName);
+            }
+
+            // Insert data into database
+            $this->paymentMethodModel->insert([
+                'name' => $this->request->getVar('name'),
+                'description' => $this->request->getVar('description'),
+                'image' => $newName 
+            ]);
+
+            return redirect()->to('/admin/payment-methods')->with('message', 'Phương thức thanh toán đã được thêm thành công.');
+        } else {
+            return redirect()->back()->withInput()->with('validation', $this->validator);
+        }
+
     }
 
     public function edit($id)
