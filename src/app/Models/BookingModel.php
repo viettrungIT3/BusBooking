6e78416700fd534857df1e33f0a9cdf9f8e957c9
viettrungIT3 <viettrungcntt03@gmail.com>
2024-05-19 12:41:55 +0700

    feat(Admin Bookings): :sparkles: getBookingsByDate in BookingModel
    Take all reservations for the specified date.

--- src/app/Models/BookingModel.php
+++ src/app/Models/BookingModel.php
@@ -23,4 +23,30 @@ class BookingModel extends Model
 
     // Rules
     // 'status'        => '[pending,confirmed,cancelled,expired,completed,refunded,failed,processing]',
+
+    /**
+     * Lấy tất cả đặt chỗ trong khoảng ngày chỉ định.
+     * 
+     * @param string $startDate Ngày bắt đầu.
+     * @param string $endDate Ngày kết thúc.
+     * @return array
+     */
+    public function getBookingsByDate($startDate, $endDate)
+    {
+        // Thêm 23 giờ, 59 phút, 59 giây vào ngày kết thúc để bao gồm cả ngày
+        $endDateTime = new \DateTime($endDate);
+        $endDateTime->setTime(23, 59, 59);
+        $formattedEndDate = $endDateTime->format('Y-m-d H:i:s');
+    
+        // Định nghĩa truy vấn lấy đặt chỗ
+        $builder = $this->db->table($this->table);
+        $builder->select('*');
+        $builder->where('created_at >=', $startDate);
+        $builder->where('created_at <=', $formattedEndDate); 
+    
+        // Thực hiện truy vấn và trả về kết quả
+        $query = $builder->get();
+        return $query->getResultArray();
+    }
+    
 }
